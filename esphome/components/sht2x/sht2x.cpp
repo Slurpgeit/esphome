f26a205377f0c87a855f19c813c3eff1e17d4719
Slurpgeit <d.maasland@gmail.com>
2023-10-30 13:35:28 +0100

    dfa

--- esphome/components/sht2x/sht2x.cpp
+++ esphome/components/sht2x/sht2x.cpp
@@ -12,7 +12,7 @@ static const uint8_t SHT2X_COMMAND_HUMIDITY = 0xF5;
 static const uint8_t SHT2X_COMMAND_SOFT_RESET = 0xFE;
 
 uint8_t SHT2XComponent::get_firmware_version() {
-  uint8_t version;
+  uint8_t version[1];
   if(!this-write_command(0x84, 0xB8)) {
     this->mark_failed();
     return 0;
